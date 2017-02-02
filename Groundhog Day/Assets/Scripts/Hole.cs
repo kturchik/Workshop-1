ec7b322049a8822404d39f929d083b4a0e75971d
klturchik <kturchik@sbcglobal.net>
2017-02-02 08:48:04 -0800

    Finished Game

---
+++ Groundhog Day/Assets/Scripts/Hole.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hole : MonoBehaviour {
+    // Mole Prefab
+    public GameObject mole;
+    public float aliveTime = 1;
+    
+    // Spawn Interval
+    public int intervalMin = 2;
+    public int intervalMax = 10;
+        
+    // Use this for initialization
+    void Start () {
+        Invoke("Spawn", Random.Range(intervalMin, intervalMax));
+    }
+    
+    void Spawn() {
+        // Spawn the mole
+        GameObject g = (GameObject)Instantiate(mole, transform.position, Quaternion.identity);
+        
+        // Make sure to destroy it after a few seconds
+        Destroy(g, aliveTime);
+        
+        // Next Spawn
+        Invoke("Spawn", Random.Range(intervalMin, intervalMax));
+    }
+}
---
+++ Groundhog Day/Assets/Scripts/Mole.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mole : MonoBehaviour {
+    // On-Hit Effect
+    public GameObject onHit;
+    private SpriteRenderer renderer;
+    private Animator anim;
+
+
+    private void Awake()
+    {
+        // get a reference to the SpriteRenderer component on this gameObject
+        renderer = GetComponent<SpriteRenderer>();
+        // animation.Blend("animation",target distance(i.e. 5.0=50% of animation),fadelength(i.e. speed of the aniamtion to blend in));
+        anim = GetComponent<Animator>();
+        
+    }
+
+    void OnMouseDown() {
+
+        // Load On-Hit effect
+        //SpriteRenderer.color = new Color(0f, 0f, 0f, 1f);
+        renderer.material.SetColor("_Color", Color.red);
+        anim.SetBool("isHit", true);
+        // Destroy it after a while
+        //Destroy(g, 0.2f);
+    }
+}
